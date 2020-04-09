4b2275d465133ee9187b240a9681b8d1cb3218db
aiborra11 <iborra.11@gmail.com>
2020-04-09 20:07:36 +0200

    ready to push]

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'producthunt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'votingapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
